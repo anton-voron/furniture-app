cdc460c89cd8a7aab3f8b39920dad01afd4b2bcb
anton-voron <anton.voron.gt@gmail.com>
2019-07-14 14:58:14 +0300

    name position

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,11 @@ const Header = (props) => {
 
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-		  <Link className="navbar-brand" to="/Catalog">Furniture App</Link>
-		  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+		  <div className = "d-flex">
+		    <Link className="navbar-brand" to="/Catalog">Furniture App</Link>
+		    <p className="name-title">Hello, {props.userName} </p>
+		  </div>
+		    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 		    <span className="navbar-toggler-icon"></span>
 		  </button>
 
@@ -17,8 +20,6 @@ const Header = (props) => {
 		        <Link className="nav-link" to="/catalog">Catalog</Link>
 		      </li>
 		    </ul>
-
-		    <p className="name-title">Hello, {props.userName} </p>
 		    <form className="form-inline my-2 my-lg-0">
 		   	  <button className="btn btn-secondary my-2 my-sm-0" onClick = {props.logOut}>Log Out</button>
 		    </form>
